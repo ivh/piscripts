b985de8e75d8a729904551d9158c1ef9e7ae87e2
Thomas Marquart <thomas@marquart.se>
2015-03-22 22:16:46 +0100

    piCam.py and webcam.py have shrunk due to moving stuff to camcommon. also better notifications

--- piCam.py
+++ piCam.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-import os
-import numpy as np
+
 import picamera
 import picamera.array
-from time import sleep
-from datetime import datetime,timedelta
+from camcommon import *
 
 #capture_size = (1296,972)
 #capture_size = (640,480)
@@ -12,37 +10,6 @@ capture_size = (864,648)
 motion_size = (640,480)
 thresh = 20
 sensit = 200
-
-SAVEDIR='/home/pi/piCam/'
-
-import pushnotify as pn
-pn.logging.basicConfig()
-NOTIFY_APPKEY = os.environ['NOTIFY_APPKEY']
-NOTIFY_USERKEY = os.environ['NOTIFY_USERKEY']
-p=pn.pushover.Client(developerkey=NOTIFY_APPKEY)
-p.apikeys={NOTIFY_USERKEY:[]}
-notifyDelta = timedelta(minutes=15)
-lastNotify = datetime.now() - notifyDelta
-
-def notify(descr,title=None,url=None,urltitle=None):
-    global lastNotify
-    if (datetime.now() - lastNotify) < notifyDelta:
-        return
-    if not title:
-        title=descr
-    if not url:
-        kwargs = None
-    else:
-        if not urltitle:
-            urltitle=url
-        kwargs={'url':url,'url_title':urltitle}
-    p.notify(description=descr,event=title,kwargs=kwargs)
-    lastNotify = datetime.now()
-
-def getFileName(prefix='motionIMG-',suffix='.jpg',dir=SAVEDIR):
-    timestring = datetime.now().isoformat()
-    timestring = timestring.replace(':','-')
-    return dir + prefix + timestring + suffix
 
 class DetectMotion(picamera.array.PiMotionAnalysis):
     def analyse(self, a):
@@ -56,8 +23,8 @@ class DetectMotion(picamera.array.PiMotionAnalysis):
             fname = getFileName()
             #camera.capture_sequence(
             #    [getFileName(suffix='')+'_%02d.jpg'%i for i in range(3)],
-            camera.capture(fname,use_video_port=True, quality = 30)
-            notify('PiCam motion!')
+            camera.capture(fname,use_video_port=True, quality = 70)
+            notify('PiCam: ' + fname.split('/')[-1], updateGallery=True)
 
 with picamera.PiCamera() as camera:
     with DetectMotion(camera, size=motion_size) as output:
@@ -65,7 +32,7 @@ with picamera.PiCamera() as camera:
         camera.framerate = 2
         camera.rotation = 180
         camera.start_preview()
-        sleep(5)
+        sleep(4)
         camera.start_recording('/dev/null',
             format='h264', motion_output=output, resize=motion_size)
 
--- webcam.py
+++ webcam.py
@@ -1,64 +1,26 @@
 #!/usr/bin/env python
-import os
-import numpy as np
 from PIL import Image
 from StringIO import StringIO
 import requests
-from time import sleep
-from datetime import datetime,timedelta
 import scipy.ndimage as ndimage
+
+from camcommon import *
 
 CAMUSR = os.environ['CAMUSR']
 CAMPWD= os.environ['CAMPWD']
 USRPWD = (CAMUSR,CAMPWD)
 #VIDURL = 'http://192.168.1.6/video.cgi'
 PICURL = 'http://192.168.1.6/image.jpg'
-SAVEDIR = '/home/pi/webcam/'
 
 DET_INTERVAL = 2
 RECORD_DURATION = 10
 THRESH = 65
 MINPIX = 1800
 
-import pushnotify as pn
-pn.logging.basicConfig()
-NOTIFY_APPKEY = os.environ['NOTIFY_APPKEY']
-NOTIFY_USERKEY = os.environ['NOTIFY_USERKEY']
-p=pn.pushover.Client(developerkey=NOTIFY_APPKEY)
-p.apikeys={NOTIFY_USERKEY:[]}
-notifyDelta = timedelta(minutes=15)
-lastNotify = datetime.now() - notifyDelta
-
-def notify(descr,title=None,url=None,urltitle=None):
-    global lastNotify
-    if (datetime.now() - lastNotify) < notifyDelta:
-        return
-    if not title:
-        title=descr
-    if not url:
-        kwargs = None
-    else:
-        if not urltitle:
-            urltitle=url
-        kwargs={'url':url,'url_title':urltitle}
-    p.notify(description=descr,event=title,kwargs=kwargs)
-    lastNotify = datetime.now()
-
-
 
 def getImage():
     req=requests.get(PICURL,auth=requests.auth.HTTPBasicAuth(*USRPWD))
     return Image.open(StringIO(req.content))
-
-def rebin_factor( a, newshape ):
-        '''Rebin an array to a new shape.
-        newshape must be a factor of a.shape.
-        '''
-        assert len(a.shape) == len(newshape)
-        assert not np.sometrue(np.mod( a.shape, newshape ))
-
-        slices = [ slice(None,None, old/new) for old,new in zip(a.shape,newshape) ]
-        return a[slices]
 
 def img2sum(img):
     chansum = np.sum(np.asarray(img,dtype='Int32'),axis=-1)
@@ -77,20 +39,13 @@ def detectMotion(curr,last):
         print 'Motion %s:'%n + datetime.now().isoformat()
     return isMoving
 
-def getFileName(prefix='motionIMG-',suffix='.jpg',dir=SAVEDIR):
-    timestring = datetime.now().isoformat().split('.')[0]
-    timestring = timestring.replace(':','-')
-    return dir + prefix + timestring + suffix
-
 def main():
-
     last=getImage()
     while True:
         curr = getImage()
         if detectMotion(curr,last):
             curr.transpose(Image.ROTATE_270).save(getFileName(),format='jpeg')
-            notify('Webcam motion')
-
+            notify('WebCam: ' + fname.split('/')[-1], updateGallery=True)
         last=curr
         sleep(DET_INTERVAL)
 
