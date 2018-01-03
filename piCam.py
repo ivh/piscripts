c517fc2fc2deb43e0b3ad949ba64c3269b53efcb
Thomas Marquart <thomas@marquart.se>
2018-01-03 20:52:12 +0100

    play with light relay and clicker

--- piCam.py
+++ piCam.py
@@ -5,9 +5,17 @@ import subprocess
 from camcommon import *
 
 import RPi.GPIO as GPIO
+#GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-MOTI = 40 # last pin is motion sensor
+MOTI = 11 # pin for motion sensor
 GPIO.setup(MOTI, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+LAMP = 40 # relay for lamp
+CLICK = 38 # empty relay for clicking sound
+LAMPON = GPIO.LOW
+LAMPOFF = GPIO.HIGH
+GPIO.setup(LAMP, GPIO.OUT,initial=LAMPOFF)
+GPIO.setup(CLICK, GPIO.OUT,initial=LAMPOFF)
+
 BOUNCE = 10 # seconds
 
 def record(camera, duration=BOUNCE, fname=None):
@@ -23,24 +31,50 @@ def record(camera, duration=BOUNCE, fname=None):
 
     camera.wait_recording(duration)
     camera.stop_recording()
-    subprocess.call(['MP4Box', '-fps', '15', '-add', fname, '%s.mp4'%fname])
+    subprocess.call(['MP4Box', '-fps', '15', '-add', fname, '%s.mp4'%fname],
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,)
     print(' done.')
 
-
-with picamera.PiCamera() as camera:
-    camera.resolution =  (640,480)
-    #camera.resolution =  (1296,972)
-    camera.framerate = 5
-    camera.rotation = 180
-    camera.start_preview()
-    camera.meter_mode='matrix'
-    camera.exposure_compensation=9
-
-    GPIO.add_event_detect(MOTI, GPIO.RISING, bouncetime=(BOUNCE+1)*1000,
-        callback=lambda chan: record(camera))
-
-    while True:
-            sleep(0.1)
+def light_click(duration=BOUNCE):
+    GPIO.output(LAMP,LAMPON)
+    Hz = 2
+    interv = 1.0/ Hz / duration
+    for i in range(Hz*duration):
+        GPIO.output(CLICK,LAMPON)
+        sleep(interv/2)
+        GPIO.output(CLICK,LAMPOFF)
+        sleep(interv/2)
+    GPIO.output(LAMP,LAMPOFF)
 
 
-GPIO.cleanup()
+# main()
+
+try:
+    with picamera.PiCamera() as camera:
+        #camera.resolution =  (640,480)
+        camera.resolution =  (1296,972)
+        camera.framerate = 3
+        camera.rotation = 180
+        camera.start_preview()
+        camera.meter_mode='matrix'
+        camera.exposure_compensation=9
+
+        GPIO.add_event_detect(MOTI, GPIO.RISING, bouncetime=BOUNCE*1000 +100)
+        GPIO.add_event_callback(MOTI, lambda chan: GPIO.output(LAMP,LAMPON))
+        GPIO.add_event_callback(MOTI, lambda chan: record(camera))
+        GPIO.add_event_callback(MOTI, lambda chan: GPIO.output(LAMP,LAMPOFF))
+        #GPIO.add_event_callback(MOTI, lambda chan: light_click())
+        #GPIO.add_event_detect(MOTI, GPIO.RISING, bouncetime=(BOUNCE+1)*1000,
+        #    callback=lambda chan: record(camera))
+
+        while True:
+                sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
+
+except:
+    pass
+
+finally:
+    GPIO.cleanup()
