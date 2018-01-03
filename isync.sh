0d468afad6cc8ef12f77050d6d4a88501f0cc576
Thomas Marquart <thomas@marquart.se>
2018-01-03 20:51:52 +0100

    only sync mp4

--- isync.sh
+++ isync.sh
@@ -2,7 +2,10 @@
 directories=( "/home/pi/cams" )
 inotifywait -q --monitor --event close_write,moved_to --format '%w%f' "${directories[@]}" | while read thing ; do
     [ -f "$thing" ] || continue
-		scp "$thing" ori:cams/  > /dev/null;
+    if [ ${thing: -4} == ".mp4" ]
+    then
+        scp "$thing" ori:cams/  > /dev/null;
 		echo "Uploaded $thing" ;
+    fi
 done
 
