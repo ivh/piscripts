bfdfbf3e7940e76ee7ef46a907f943862857cc6d
Thomas Marquart <thomas@marquart.se>
2018-01-04 11:20:15 +0100

    get diplay to work and tune the motion trigger with lamp

--- piCam.py
+++ piCam.py
@@ -4,25 +4,34 @@ import picamera
 import subprocess
 from camcommon import *
 
+import Adafruit_SSD1306
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=24)
+from PIL import Image, ImageDraw, ImageFont
+image = Image.new('1', (disp.width, disp.height))
+draw = ImageDraw.Draw(image)
+font = ImageFont.load_default()
+draw.text((0,30),'Recording & Uploading!', fill=255, font=font)
+
 import RPi.GPIO as GPIO
 #GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-MOTI = 11 # pin for motion sensor
-GPIO.setup(MOTI, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-LAMP = 40 # relay for lamp
-CLICK = 38 # empty relay for clicking sound
+#GPIO.setmode(GPIO.BCM) # Set by display lib already
 LAMPON = GPIO.LOW
 LAMPOFF = GPIO.HIGH
-GPIO.setup(LAMP, GPIO.OUT,initial=LAMPOFF)
-GPIO.setup(CLICK, GPIO.OUT,initial=LAMPOFF)
+MOTI = 17 # pin for motion sensor
+LAMP = 21 # relay for lamp
+CLICK = 20 # empty relay for clicking sound
 
-BOUNCE = 10 # seconds
+BOUNCE = 9 # seconds
 
-def record(camera, duration=BOUNCE, fname=None):
+
+def record(camera, disp=disp, image=image, duration=BOUNCE, fname=None):
     if not fname:
         fname=getFileName(prefix='cam',suffix='h264')
     dirname,basename = os.path.split(fname)
 
+    GPIO.output(LAMP,LAMPON)
+    disp.image(image)
+    disp.display()
     camera.start_recording(fname)
     print('Recording to %s ...'%fname, end='', flush=True)
 
@@ -31,25 +40,22 @@ def record(camera, duration=BOUNCE, fname=None):
 
     camera.wait_recording(duration)
     camera.stop_recording()
+    GPIO.output(LAMP,LAMPOFF)
     subprocess.call(['MP4Box', '-fps', '15', '-add', fname, '%s.mp4'%fname],
             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,)
+    disp.clear()
+    disp.display()
     print(' done.')
 
-def light_click(duration=BOUNCE):
-    GPIO.output(LAMP,LAMPON)
-    Hz = 2
-    interv = 1.0/ Hz / duration
-    for i in range(Hz*duration):
-        GPIO.output(CLICK,LAMPON)
-        sleep(interv/2)
-        GPIO.output(CLICK,LAMPOFF)
-        sleep(interv/2)
-    GPIO.output(LAMP,LAMPOFF)
-
-
-# main()
-
 try:
+    GPIO.setup(MOTI, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(LAMP, GPIO.OUT,initial=LAMPOFF)
+    GPIO.setup(CLICK, GPIO.OUT,initial=LAMPOFF)
+
+    disp.begin()
+    disp.clear()
+    disp.display()
+
     with picamera.PiCamera() as camera:
         #camera.resolution =  (640,480)
         camera.resolution =  (1296,972)
@@ -57,15 +63,11 @@ try:
         camera.rotation = 180
         camera.start_preview()
         camera.meter_mode='matrix'
-        camera.exposure_compensation=9
+        camera.exposure_compensation=6
 
-        GPIO.add_event_detect(MOTI, GPIO.RISING, bouncetime=BOUNCE*1000 +100)
-        GPIO.add_event_callback(MOTI, lambda chan: GPIO.output(LAMP,LAMPON))
-        GPIO.add_event_callback(MOTI, lambda chan: record(camera))
-        GPIO.add_event_callback(MOTI, lambda chan: GPIO.output(LAMP,LAMPOFF))
-        #GPIO.add_event_callback(MOTI, lambda chan: light_click())
-        #GPIO.add_event_detect(MOTI, GPIO.RISING, bouncetime=(BOUNCE+1)*1000,
-        #    callback=lambda chan: record(camera))
+        GPIO.add_event_detect(MOTI, GPIO.RISING, bouncetime=(BOUNCE+1)*1000,
+                    callback=lambda chan: record(camera))
+        #GPIO.add_event_callback(MOTI, lambda chan: record(camera))
 
         while True:
                 sleep(0.1)
@@ -77,4 +79,6 @@ except:
     pass
 
 finally:
+    disp.clear()
+    disp.display()
     GPIO.cleanup()
